348da01aeb7afc7a54f5ea4a1a8e1ae9dbbeafcd
Serge Kudashev <kudashevs@gmail.com>
2019-10-11 14:02:07 +0300

    Shorten Calculator test with mock

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -4,12 +4,7 @@ namespace CalculatorViaInterface\Tests;
 
 use PHPUnit\Framework\TestCase;
 use CalculatorViaInterface\Calculator;
-use CalculatorViaInterface\Operations\Modulus;
 use CalculatorViaInterface\Operations\Addition;
-use CalculatorViaInterface\Operations\Division;
-use CalculatorViaInterface\Operations\Subtraction;
-
-use CalculatorViaInterface\Operations\Multiplication;
 
 class CalculatorTest extends TestCase
 {
@@ -37,57 +32,20 @@ class CalculatorTest extends TestCase
         $calculator = new Calculator(new Addition(), 'x', 0);
     }
 
-    public function testCalculateAddition()
+    public function testOperableWorksAsExpectedWhenMock()
     {
-        $calculator = new Calculator(new Addition(), 2, 2);
-        $this->assertEquals(4, $calculator->calculate());
-        $calculator = new Calculator(new Addition(), 5, 2);
-        $this->assertEquals(7, $calculator->calculate());
-    }
+        $mock = $this->getMockBuilder(Addition::class)
+            ->setMethods(['check', 'handle'])
+            ->getMock();
+        $mock->expects($this->once())
+            ->method('handle')
+            ->with(
+                $this->equalTo(2),
+                $this->equalTo(2)
+            )
+            ->willReturn(4);
 
-    public function testCalculateSubstraction()
-    {
-        $calculator = new Calculator(new Subtraction(), 2, 2);
-        $this->assertEquals(0, $calculator->calculate());
-        $calculator = new Calculator(new Subtraction(), 5, 2);
-        $this->assertEquals(3, $calculator->calculate());
-    }
-
-    public function testCalculateMultiplication()
-    {
-        $calculator = new Calculator(new Multiplication(), 2, 2);
-        $this->assertEquals(4, $calculator->calculate());
-        $calculator = new Calculator(new Multiplication(), 5, 2);
-        $this->assertEquals(10, $calculator->calculate());
-    }
-
-    public function testCalculateDivision()
-    {
-        $calculator = new Calculator(new Division(), 2, 2);
-        $this->assertEquals(1, $calculator->calculate());
-        $calculator = new Calculator(new Division(), 5, 2);
-        $this->assertEquals(2.5, $calculator->calculate());
-    }
-
-    public function testCalculateDivisionZeroException()
-    {
-        $calculator = new Calculator(new Division(), 2, 0);
-        $this->expectException('\DivisionByZeroError');
-        $calculator->calculate();
-    }
-
-    public function testCalculateModulus()
-    {
-        $calculator = new Calculator(new Modulus(), 2, 2);
-        $this->assertEquals(0, $calculator->calculate());
-        $calculator = new Calculator(new Modulus(), 5, 2);
-        $this->assertEquals(1, $calculator->calculate());
-    }
-
-    public function testCalculateModulusZeroException()
-    {
-        $calculator = new Calculator(new Modulus(), 2, 0);
-        $this->expectException('\DivisionByZeroError');
-        $calculator->calculate();
+        $calc = new Calculator($mock, 2, 2);
+        $this->assertEquals(4, $calc->calculate());
     }
 }
