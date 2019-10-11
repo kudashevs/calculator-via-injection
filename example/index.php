c3871dc96248cdf47d0b5fdab04e62627235fc51
Serge Kudashev <kudashevs@gmail.com>
2019-10-11 13:40:21 +0300

    Update example

--- example/index.php
+++ example/index.php
@@ -1,26 +1,25 @@
 <?php
 
-use CalculatorViaInterface\CalculatorGenerator;
+namespace CalculatorViaInterface;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use CalculatorViaInterface\Operations\Addition;
 use CalculatorViaInterface\Operations\Division;
 
-require_once 'vendor/autoload.php';
-
 try {
-    $calculator = new CalculatorGenerator(new Addition(), 1, 2);
-    echo $calculator->calculate() . PHP_EOL; // 3
-} catch (\TypeError $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo 'Wrong arguments type passed to constructor!' . PHP_EOL;
+    $calc = new Calculator(new Addition(), 1, 2);
+    echo $calc->calculate() . PHP_EOL; // 3
+} catch (\Exception $e) {
+    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
+    echo $e->getMessage() . PHP_EOL;
 }
 
+
 try {
-    $calculator = new CalculatorGenerator(new Division(), 1, 2);
-    echo $calculator->calculate() . PHP_EOL; // 0.5
-} catch (\DivisionByZeroError $e) {
+    $calc = new Calculator(new Division(), 1, 2);
+    echo $calc->calculate() . PHP_EOL; // 0.5
+} catch (\Exception $e) {
     error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
-    echo 'Division failed due to division by zero error!' . PHP_EOL;
-} catch (\TypeError $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo 'Wrong arguments type passed to constructor!' . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
 }
