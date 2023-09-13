f8906035f5c07afd507fa2ad94464a98feeb3a18
Sergey Kudashev <kudashevs@gmail.com>
2023-09-13 14:27:29 +0300

    Style examples fix code style

--- example/index.php
+++ example/index.php
@@ -11,15 +11,14 @@ try {
     $calc = new Calculator(new Addition(), 1, 2);
     echo $calc->calculate() . PHP_EOL; // 3
 } catch (\Exception $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
+    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
     echo $e->getMessage() . PHP_EOL;
 }
-
 
 try {
     $calc = new Calculator(new Division(), 1, 2);
     echo $calc->calculate() . PHP_EOL; // 0.5
 } catch (\Exception $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
+    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
     echo $e->getMessage() . PHP_EOL;
 }
