82ab9b02f120d4798000970ba5ee41c371871670
Sergey Kudashev <kudashevs@gmail.com>
2023-09-24 19:58:49 +0300

    Refactor examples extract an explainException function

--- examples/index.php
+++ examples/index.php
@@ -11,14 +11,23 @@ try {
     $addition = new Calculator(new Addition());
     echo $addition->calculate(1, 2) . PHP_EOL; // results in 3
 } catch (\Exception $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo $e->getMessage() . PHP_EOL;
+    echo explainException($e);
 }
 
 try {
     $division = new Calculator(new Division());
     echo $division->calculate(1, 2) . PHP_EOL; // results in 0.5
 } catch (\Exception $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo $e->getMessage() . PHP_EOL;
+    echo explainException($e);
+}
+
+function explainException(\Throwable $exception): string
+{
+    return sprintf(
+        '%s: Something went wrong with a message "%s" in file %s on line %s.' . PHP_EOL,
+        get_class($exception),
+        $exception->getMessage(),
+        $exception->getFile(),
+        $exception->getLine(),
+    );
 }
